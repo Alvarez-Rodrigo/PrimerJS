1f2b7034cd51cba1d1285fe0e639a0ef22ab29bb
Alvarez Rodrigo <alvarez.prodrigo@gmail.com>
2023-06-13 21:47:29 -0300

    modificando

--- script.js
+++ script.js
@@ -1,133 +1,4 @@
 /*
-    (OK)Usuarios: objetos (nombre, edad, califican)
-    (OK)Generar un Array de usuarios.
-
-    Elegir usuario:
-        -Poder jugar.
-        -Salir del juego.
-        -Guardar juegos.
-    Salir del usuario.
-    
-    
-*/
-
-
-
-alert("Bienvenido a tus JuegosProtatiles! ")
-//funcion para validar la edad ingresada.
-
-let mensaje = "Ingresa tu edad";
-function validarNumero(numero, mensaje){
-    while(isNaN(numero)){
-        alert("Ingresaste un valor no acptado. Vuelve a intentar.")
-        numero = parseInt(prompt(mensaje));
-    } 
-    return numero;
-}
-
-function iniciar(){
-    //Muestro a todos los Usuarios ya registrados.
-    let mensaje = "Estos son los usuarios registrados, ingrese el ID que corresponda.  \n"
-    USUARIOS.forEach(element => {
-        mensaje += `${element.id} - ${element.nombre} \n`
-    });
-    //Analizamos la respuesta.
-    let respuesta = parseInt(prompt( `elegi un usuario` ));
-    respuesta = validarNumero(respuesta, mensaje);
-
-    let usuarioElegido= USUARIOS.find(element => element.id === respuesta);
-
-    programa(usuarioElegido);
-};
-
-//Funcion para dar inicio o registrar usuario.
-function usuarioRegistrado(parametro){
-    if ((parametro == "si") || (parametro == "sí")){
-        iniciar();
-    }else if(parametro =="no"){
-        registrarse();
-    }else{
-        primeraRespuesta = prompt("Tenes un usuario iniciado? (si/no)").toLocaleLowerCase();
-        usuarioRegistrado(primeraRespuesta)
-    }
-};
-
-
-//funcion para registrar un nuevo usuario.
-function registrarse() {
-    let nombreUsuario = prompt("Como es tu nombre? ")
-    let edadUsuario = parseInt(prompt("Cuantos anios tenes ? "))
-    edadUsuario = validarNumero(edadUsuario, mensaje); 
-    let num = USUARIOS.length + 1;
-    USUARIOS.push(num ,nombreUsuario, edadUsuario);
-    alert("usuario registrado.")
-    programa();
-}
-
-function programa (participante){
-    let seguir = true;
-
-    while(seguir){
-        //opciones dentro del programa.
-
-        let num = parseInt(prompt("eleji cualquiera de las siguientes opciones:\n 1.iniciar juego \n 2. salier del usuario. \n 3.Guardar juego. \n "));
-
-        switch(num){
-            case 1:
-                alert("Iniciaste el juego.");
-                juego();
-            break;
-            case 2:
-                alert("Saliste del juego.");
-                seguir = false;
-            break;
-            case 3:
-                participante.guardado = new Date();
-            break;
-            default: 
-                alert("Tenes que eleguir un numero del 1 al 3.");
-            break;
-        }
-    }
-    promedioEdad();
-}
-function juego(){
-    let numeroUser = parseInt(prompt("Decime un numero del 1 al 100"));
-
-    let numeroPC = Math.random() * 100; 
-
-    numeroPC = Math.ceil(numeroPC); //Math.round
-
-    if (numeroUser > numeroPC){
-        alert(`Ganaste con ${numeroUser} a mi ${numeroPC} `)        
-    }else if (numeroUser == numeroPC){
-            alert("Encontraste el empate!!")
-    }else{
-            alert(`No le pudiste ganar al ${numeroPC} de la PC`)
-    };
-}
-//Sacamos la edad promedio de los usuarios.
-function promedioEdad (){
-    let acumulador = 0;
-
-    USUARIOS.forEach(element => {
-        acumulador += element.edad
-    });
-
-    let promedio = (acumulador/USUARIOS.length);
-    console.log(`La edad promedio es de ${promedio} `)
-}
-
-//constrctor de Usuario
-class Usuario {
-    constructor(id, nombre, edad){
-        this.id =id,
-        this.nombre =nombre,
-        this.edad =edad,
-        this.guardado = "";
-    }
-}
-
 const usuario1 = new Usuario (1, "Ramon Remes", 7);
 const usuario2 = new Usuario (2, "Raquel Tere", 9);
 const usuario3 = new Usuario (3, "Pepe pepines", 6);
@@ -135,8 +6,151 @@ const usuario4 = new Usuario (4, "Sol Solias", 10);
 const usuario5 = new Usuario (5, "Nestor Neres", 10);
 
 const USUARIOS = [usuario1, usuario2, usuario3, usuario4, usuario5];
+    
+    
+*/
 
-let primeraRespuesta = prompt("Tenes un usuario iniciado? (si/no)").toLocaleLowerCase();
+const usuarios = [
+    {nombre: 'Ramon remes', edad: 7},
+    {nombre: 'Raquel Tere', edad: 8},
+    {nombre: 'Pepe Pepines', edad: 10},
+    {nombre: 'Sol Solari', edad: 18},
+    {nombre: 'Nestor Nemes', edad: 7},
+    {nombre: 'Tata Martino', edad: 9},
+    {nombre: 'Lobelto Petinato', edad: 6},
+    {nombre: 'Shusi Tamara', edad: 10}
+];
 
-usuarioRegistrado(primeraRespuesta);
+const inicio = document.getElementById('inicio');
+const registrar = document.getElementById('formulario');
+const usuariosContainer = document.getElementById('usuarios');
+const promedioEdadesContainer = document.getElementById('promedioEdades');
 
+
+const darInicio = () => {
+    const botonInicio = document.createElement('button');
+    botonInicio.textContent = 'Iniciar';
+    botonInicio.addEventListener('click', () => {
+        agregarBoxes(usuarios);
+        botonInicio.disabled = true;/*Hace que no se pueda pulsar mas de una vez el inicio*/ });
+    inicio.appendChild(botonInicio);
+};
+
+const registrarse = () => {//Crea el boton de registro.
+    const botonRegistro = document.createElement('button');
+    botonRegistro.textContent = 'Registrarse';
+    botonRegistro.addEventListener('click', () => {
+        mostrarFormulario();
+        botonRegistro.disabled = true;
+    });
+    inicio.appendChild(botonRegistro);
+};
+
+const mostrarFormulario = () => {
+    const formulario = document.createElement('form');
+    formulario.addEventListener('submit', (event) => {
+        event.preventDefault(); // Evitar el envío del formulario
+        const nombre = event.target.nombre.value;
+        const edad = parseInt(event.target.edad.value);
+
+        if (nombre && edad) {
+            const nuevoUsuario = { nombre, edad };
+            usuarios.push(nuevoUsuario);
+            const boxNuevoUsuario = creadorBoxes(nuevoUsuario);
+            usuariosContainer.appendChild(boxNuevoUsuario);
+            event.target.reset(); // Limpiar el formulario
+            calcularPromedioEdades();
+
+        }
+    });
+
+    const inputNombre = document.createElement('input');
+    inputNombre.type = 'text';
+    inputNombre.name = 'nombre';
+    inputNombre.placeholder = 'Nombre';
+
+    const inputEdad = document.createElement('input');
+    inputEdad.type = 'number';
+    inputEdad.name = 'edad';
+    inputEdad.placeholder = 'Edad';
+
+    const botonRegistrar = document.createElement('button');
+    botonRegistrar.textContent = 'Registrar';
+    botonRegistrar.addEventListener('click', () => {
+        const nombre = inputNombre.value;
+        const edad = parseInt(inputEdad.value);
+
+        if (nombre && edad) {
+            const nuevoUsuario = { nombre, edad };
+            usuarios.push(nuevoUsuario);
+            const boxNuevoUsuario = creadorBoxes(nuevoUsuario);
+            usuariosContainer.appendChild(boxNuevoUsuario);
+            formulario.reset(); // Limpiar el formulario
+            calcularPromedioEdades();
+            guardarUsuarios(); // Guardar los usuarios en el localStorage
+        }
+    });
+
+    formulario.appendChild(inputNombre);
+    formulario.appendChild(inputEdad);
+    formulario.appendChild(botonRegistrar);
+
+    registrar.appendChild(formulario);
+    
+};
+
+const creadorBoxes = (usuario) => {
+    const box = document.createElement('div'); //crea la caja
+    box.classList.add('box');
+
+    const nombre = document.createElement('p');//Escribe el nombre
+    nombre.textContent = `Nombre: ${usuario.nombre}`;
+
+    const edad = document.createElement('p');//Escribe la edad
+    edad.textContent = `Edad: ${usuario.edad}`;
+
+    box.appendChild(nombre);
+    box.appendChild(edad);
+    
+    return box;
+};
+
+const agregarBoxes = (usuarios) => {
+    const boxes = usuarios.map((usuario) => {
+        return creadorBoxes(usuario);
+    });
+    inicio.append(...boxes);
+    calcularPromedioEdades();
+    
+};
+
+const calcularPromedioEdades = () => {
+    const edades = usuarios.map((usuario) => usuario.edad);
+    const totalEdades = edades.reduce((acumulador, edad) => acumulador + edad, 0);
+    const promedioEdades = totalEdades / usuarios.length;
+
+    promedioEdadesContainer.textContent = `Promedio de edades: ${promedioEdades.toFixed(2)}`;
+};
+
+const guardarUsuarios = () => {
+    localStorage.setItem('usuarios', JSON.stringify(usuarios));
+  };
+  
+function obtenerUsuariosGuardados() {
+    const usuariosJSON = localStorage.getItem('usuarios');
+    return JSON.parse(usuariosJSON);
+};
+
+
+/*const iniciarAplicacion = () => {
+    const usuariosGuardados = obtenerUsuariosGuardados();
+    if (usuariosGuardados) {
+        usuarios.push(...usuariosGuardados); // Agregar los usuarios guardados al array
+        agregarBoxes(usuariosGuardados); // Mostrar los usuarios guardados en los boxes
+        calcularPromedioEdades(); // Calcular el promedio de edades
+    }
+};*/
+
+darInicio();
+//iniciarAplicacion();
+registrarse();
