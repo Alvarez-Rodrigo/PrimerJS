a76a68437a32d4f90ed2599eb75265a6b3e1a7ae
Alvarez Rodrigo <alvarez.prodrigo@gmail.com>
2023-07-14 18:28:37 -0300

    modificaciones

--- class.js
+++ class.js
@@ -1,7 +1,26 @@
 class Pelicula {
-    constructor(id, nombre, imagen) {
+    constructor(id, nombre, precio, disponible, cantidad, imagen) {
             this.id = id,
             this.nombre = nombre,
+            this.precio = precio,
+            this.disponible = disponible,
+            this.cantidad = cantidad,
             this.imagen = imagen
     }
+    restaEntradasDisponibles(){
+        this.disponibles = this.disponibles - 1;
+      }
 }
+
+const pelicula1 = new Pelicula (0, "batman", 100, 10, 1, "./img/batman.jpg")
+const pelicula2 = new Pelicula (1,"superman", 140, 40, 1, "./img/superman.jpg")
+const pelicula3 = new Pelicula (2,"pokemon", 100, 5, 1, "./img/pokemon.jpg")
+const pelicula4 = new Pelicula (3,"mouster house", 50, 15, 1, "./img/monsterhouse.jpg")
+const pelicula5 = new Pelicula (4,"charli y la fabrica de chocolate", 90, 100, 1, "./img/charlie.jpg")
+
+//Array de peliculas
+const listaPeliculas = [pelicula1, pelicula2, pelicula3, pelicula4, pelicula5];
+
+
+//Guardar array
+localStorage.setItem("Peliculas", JSON.stringify(listaPeliculas))
--- script.js
+++ script.js
@@ -1,44 +1,185 @@
 const inicio = document.getElementById('inicio');
 const cartelera = document.getElementById('cartelera');
 
-function renderizarPeliculas(peliculasMostrar) {
-  const divPelicula = document.getElementById('cartelera');
+function traerPeliculas(){
+  fetch('./peliculas.json')
+  .then(res => res.json())
+  .then(data => mostrarPeliculas(data))
+}
 
-  divPelicula.innerHTML = '';
+//Cargamos la información sobre los espectáculos cuando se inicia el DOM
+document.addEventListener("DOMContentLoaded", ()=>{
+  traerPeliculas()
+})
 
-  peliculasMostrar.forEach((pelicula) => {
-    const cardDiv = document.createElement('div');
-    cardDiv.classList.add('box');
-    cardDiv.innerHTML = `
-        <h2>${pelicula.nombre}</h2>
-        <img src="./img/${pelicula.imagen}" alt="${pelicula.nombre}">
-    `;
-    divPelicula.appendChild(cardDiv);
+function mostrarPeliculas(PeliculasEnJSON) {
+  for (let pelicula of PeliculasEnJSON) {
+    let contenedor = document.createElement("div")
+    contenedor.innerHTML = `
+     <div class="card cardPropia" style="width: 18rem; font-family: 'Comfortaa'">
+     <img src=${pelicula.imagen} class="card-img-top" alt="ImagenDepelicula">
+       <div class="card-body micard">
+         <h4 class="card-title"><strong>${pelicula.nombre}</strong></h4>
+         <p class="card-text"><strong>Precio por entrada: $${pelicula.precio}</strong></p>
+         <input type="button" class="btn btn-primary" value="Comprar" onclick="agregarCarrito(${pelicula.id})"> 
+       </div>
+     </div>`
+    contenedor.classList.add("miCard");
+    cartelera.append(contenedor)
+  }
+}
+
+
+//Formulario para logear
+const btnVerWeb = document.querySelector("#botonWeb")
+btnVerWeb.onclick = () => {
+    document.getElementById("formulario").style.display = "flex";
+}
+//Datos para guardar el logeo
+const datosFormulario = document.getElementById("formulario");
+const btnSubmit = document.getElementById("btnSubmit");
+
+btnSubmit.onclick = () => {
+  let nombreUser = datosFormulario.querySelector("#nombreUser").value;
+  localStorage.setItem("nombreUser", nombreUser);
+  let mailUser = datosFormulario.querySelector("#mailUser").value;
+  localStorage.setItem("mailUser", mailUser);
+  if (nombreUser !== "" && mailUser !== "") {
+    Swal.fire(
+      '¡Muchas gracias!',
+      'Te suscribiste a nuestra Web.',
+      'success'
+    );
+    document.getElementById("formulario").style.display = "none";
+  } else {
+    Swal.fire(
+      'Error en los datos invalido.',
+      'Por favor, volve a ingresarlos',
+      'error'
+    );
+  }
+};
+
+datosFormulario.onsubmit = (e) => {
+  e.preventDefault();
+};
+
+//carrito de compras
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+const agregarCarrito = (pelicula) => {
+  Toastify({
+      text: "¡Agregaste 1 entrada a tu carrito!",
+      duration: 2000,
+      newWindow: true,
+      close: true,
+      gravity: "top", 
+      position: "center",
+      stopOnFocus: true,
+      style: {
+        background: "rgb(3, 189, 118)",
+      },
+      onClick: function(){}
+    }).showToast();
+  fetch("./peliculas.json")
+    .then(res => res.json())
+    .then(data => {
+      const peliculaEnCarrito = carrito.find((item) => item.id === pelicula);
+      if (peliculaEnCarrito) {
+        peliculaEnCarrito.cantidad++;
+        } else {
+          const pelicualNuevo = data.find((item) => item.id === pelicual);
+          carrito.push(pelicualNuevo);
+        }
+  actualizarCarrito();
+  localStorage.setItem("carrito", JSON.stringify(carrito))
   });
-}
+};
 
-function verCartelera() {
-  const request = new XMLHttpRequest();
-  request.open('GET', 'peliculas.json', true);
+const actualizarCarrito = () => {
+  carritoCompra.innerHTML = '';
+    carrito.forEach((pelicula) => {
+        const contenedor = document.createElement('div');
+        contenedor.classList.add('peliculaEnCarrito');
+        const mensaje = document.createElement('div');
+        mensaje.innerText = `Agregaste ${pelicula.cantidad} entrada/s para ${pelicula.nombre} - Costo total: (${pelicula.precio*pelicula.cantidad})`;
+        const cantidad = document.createElement('p');
+        cantidad.textContent = pelicula.cantidad;
+        const botonEliminar = document.createElement('button');
+        botonEliminar.textContent = 'Eliminar';
+        botonEliminar.addEventListener('click', () => {
+          eliminarCarrito(pelicula);
+            Toastify({
+              text: "Eliminaste 1 entrada de tu carrito",
+              duration: 2000,
+              newWindow: true,
+              close: true,
+              gravity: "top", 
+              position: "center",
+              stopOnFocus: true,
+              style: {
+                background: "red",
+              },
+              onClick: function(){}
+            }).showToast();
+        })
+        contenedor.appendChild(mensaje);  
+        contenedor.appendChild(cantidad);
+        contenedor.appendChild(botonEliminar);
+        
+      carritoCompra.appendChild(contenedor);
+    });
+  }
 
-  request.onload = function () {
-    if (request.status >= 200 && request.status < 400) {
-      const data = JSON.parse(request.responseText);
-      const peliculas = data.map((peliculaData) => new Pelicula(peliculaData.id, peliculaData.nombre, peliculaData.imagen));
-      renderizarPeliculas(peliculas);
-    } else {
-      console.error('Error al cargar los datos de las películas:', request.status, request.statusText);
+  const eliminarCarrito = (pelicula) => {
+    if (pelicula.cantidad > 1) {
+      pelicula.cantidad--;
+    }else{
+        const index = carrito.indexOf(pelicula);
+        carrito.splice(index, 1);
     }
-  };
-
-  request.onerror = function () {
-    console.error('Error al cargar los datos de las películas');
-  };
-
-  request.send();
+    actualizarCarrito()
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const verCarteleraButton = document.getElementById('verCartelera');
-  verCarteleraButton.addEventListener('click', verCartelera);
-});
+//Para ver el carrito
+const verCarrito = document.querySelector("#verCarrito")
+verCarrito.onclick = () => {
+  carritoCompra.style.display = "flex";
+  finalizaCompra.style.display = "flex";
+}
+
+const finalizaCompra = document.getElementById("finalizaCompra")
+const finalizarCompra = document.createElement("div")
+finalizarCompra.innerHTML += `
+      <input type="button" value="Finalizar Compra" class="btn btn-primary" onClick="terminarCompra()">
+      <input type="button" value="Vaciar Carrito" class="btn btn-primary" onClick="vaciarCarrito()">`
+finalizaCompra.append(finalizarCompra)
+
+function terminarCompra(){
+  swal.fire({
+    title: "¡Muchas gracias!",
+    text: `Se realizo el pago`,
+    icon: "success",
+    timer: 3000
+  }) 
+  localStorage.removeItem("carrito")
+  carrito.pop()
+  carritoCompra.innerHTML = ``
+  document.getElementById("finalizaCompra").style.display = "none";
+  document.getElementById("carritoCompra").style.display = "none";
+} 
+
+function vaciarCarrito(){
+    while(carrito.length > 0){
+        carrito.pop()
+    }
+    Swal.fire({
+      title: '¡Eliminado!',
+      text: 'Vaciaste tu carrito.',
+      icon: 'success',
+    })
+    localStorage.removeItem("carrito")
+    carritoCompra.innerHTML = ``
+  document.getElementById("finalizaCompra").style.display = "none";
+  document.getElementById("carritoCompra").style.display = "none";
+} 
